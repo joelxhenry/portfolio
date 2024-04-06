1b6bb7a755a5c1f1f65db3f4b478abe781d395cd
Joel Henry <hjoel.global@gmail.com>
2024-04-06 15:19:30 -0500

    Websites updates

--- app/content/projects.ts
+++ app/content/projects.ts
@@ -14,25 +14,23 @@ export interface ProjectInterface {
 const projects: ProjectInterface[] = [
   {
     title: "Written Character Recognition",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque provident autem, ullam expedita incidunt quidem in minima maiores quos consectetur laudantium vel! Molestiae fuga, voluptatem alias ducimus optio eius amet.`,
+    description: `
+    The project revolves around creating a straightforward tool for recognizing handwritten characters. 
+    Implemented with Python and Next.js, it employs a Convolutional Neural Network (CNN) to identify uppercase alphabetical characters. 
+    Through meticulous training using labeled data, the CNN model achieves reliable recognition accuracy.`,
     image:
       "https://www.securescan.com/wp-content/uploads/2022/01/OCR-355x510.gif",
-    preview_link: "#",
-    source_code_link: "#",
-    stack: ["Nextjs", "Python", "Typescript"],
-  },
-  {
-    title: "Glamorous Bundles",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque provident autem, ullam expedita incidunt quidem in minima maiores quos consectetur laudantium vel! Molestiae fuga, voluptatem alias ducimus optio eius amet.`,
-    image:
-      "https://www.securescan.com/wp-content/uploads/2022/01/OCR-355x510.gif",
-    preview_link: "#",
-    source_code_link: "#",
-    stack: ["Nextjs", "Python", "Typescript"],
+    preview_link: "https://hand-written-character-recognition.onrender.com",
+    source_code_link:
+      "https://github.com/joelxhenry/Hand-Written-Character-Recognition.git",
+    stack: ["Nextjs", "Python", "Typescript", "TensorFlow", "Kaggle Dataset"],
   },
   {
     title: "Autozone Dealership Management System (ADMS)",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque provident autem, ullam expedita incidunt quidem in minima maiores quos consectetur laudantium vel! Molestiae fuga, voluptatem alias ducimus optio eius amet.`,
+    description: `
+    The Autozone DMS is a comprehensive car dealership management system designed to streamline business operations. 
+    It empowers business owners to efficiently organize their inventory and simplifies essential tasks such as 
+    advertising, generating invoices, and managing accounting processes.`,
     image:
       "https://www.securescan.com/wp-content/uploads/2022/01/OCR-355x510.gif",
     preview_link: "#",
@@ -41,16 +39,23 @@ const projects: ProjectInterface[] = [
   },
   {
     title: "Dominoes",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque provident autem, ullam expedita incidunt quidem in minima maiores quos consectetur laudantium vel! Molestiae fuga, voluptatem alias ducimus optio eius amet.`,
+    description: `
+     This project was an exciting challenge of digitizing a cherished Caribbean pastime: dominoes. 
+     This digital adaptation of the game introduces real-time functionality, 
+     allowing two players to engage in lively matches against each other.`,
     image:
-      "https://www.securescan.com/wp-content/uploads/2022/01/OCR-355x510.gif",
+      "https://st2.depositphotos.com/19581100/47229/i/450/depositphotos_472296994-stock-photo-playing-dominoes-orange-table-domino.jpg",
     preview_link: "#",
     source_code_link: "#",
     stack: ["Nextjs", "Python", "Typescript"],
   },
   {
     title: "Zeen",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque provident autem, ullam expedita incidunt quidem in minima maiores quos consectetur laudantium vel! Molestiae fuga, voluptatem alias ducimus optio eius amet.`,
+    description: `
+    Zeen presents a straightforward social platform crafted for small business owners eager to showcase their 
+    offerings. It provides a convenient reservation system, enabling customers to schedule appointments based on 
+    the availability of account holders. Ideal for service professionals such as barbers and hairdressers, Zeen 
+    streamlines the booking process and enhances visibility for businesses.`,
     image:
       "https://www.securescan.com/wp-content/uploads/2022/01/OCR-355x510.gif",
     preview_link: "#",
@@ -59,9 +64,12 @@ const projects: ProjectInterface[] = [
   },
   {
     title: "Tic-Tac-Toe",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque provident autem, ullam expedita incidunt quidem in minima maiores quos consectetur laudantium vel! Molestiae fuga, voluptatem alias ducimus optio eius amet.`,
+    description: `
+    The Tic Tac Toe project is a classic implementation of the well-known game, employing the basic features of HTML, 
+    CSS, and JavaScript. This simple yet engaging project serves as an excellent starting point for developers 
+    to grasp fundamental web development concepts.`,
     image:
-      "https://www.securescan.com/wp-content/uploads/2022/01/OCR-355x510.gif",
+      "https://img.freepik.com/premium-vector/tictactoe-game-icon-vector-design-concept-illustration_855620-328.jpg",
     preview_link: "#",
     source_code_link: "#",
     stack: ["Nextjs", "Python", "Typescript"],
--- app/sections/projects.tsx
+++ app/sections/projects.tsx
@@ -14,13 +14,17 @@ import {
   CardBody,
   HStack,
   Button,
+  Link,
+  List,
+  ListItem,
+  ListIcon,
 } from '@chakra-ui/react'
 import Skills from './skills'
 import projects, { ProjectInterface } from '../content/projects'
 import ColorScheme from '../assets/colors'
 import { Content } from './header'
 import Slider from 'react-slick'
-import { Code, PlayOne } from '@icon-park/react'
+import { ArrowRight, Code, PlayOne } from '@icon-park/react'
 
 function SearchInput() {
   return (
@@ -37,107 +41,118 @@ function SearchInput() {
 
 function Project({ project }: { project: ProjectInterface }) {
   return (
-    <Box>
-      <Card
-        shadow={'md'}
-        rounded={10}
-        bg={useColorModeValue('whiteAlpha.400', 'blackAlpha.400')}
-        backdropBlur={'md'}
-        border={`0.5px solid ${useColorModeValue(
-          ColorScheme.light.primary,
-          ColorScheme.dark.primary,
-        )}`}
-      >
-        <CardBody>
-          <Grid
-            alignItems={'center'}
-            templateColumns={'repeat(12,1fr)'}
-            gap={10}
-          >
-            <GridItem colSpan={{ base: 12, md: 4, lg: 5 }}>
-              <Image
-                shadow={'md'}
-                rounded={10}
-                src={project.image}
-                alt={project.title}
-              />
-            </GridItem>
-            <GridItem colSpan={{ base: 12, md: 8, lg: 7 }}>
-              <Heading mb={10} fontSize={'lg'}>
+    <Card
+      shadow={'md'}
+      h={'full'}
+      rounded={10}
+      bg={useColorModeValue('whiteAlpha.400', 'blackAlpha.400')}
+      backdropBlur={'md'}
+      border={`0.5px solid ${useColorModeValue(
+        ColorScheme.light.primary,
+        ColorScheme.dark.primary,
+      )}`}
+    >
+      <CardBody>
+        <Grid alignItems={'center'} templateColumns={'repeat(12,1fr)'} gap={10}>
+          <GridItem colSpan={{ base: 12, md: 4, lg: 5 }}>
+            <Image
+              shadow={'md'}
+              rounded={10}
+              src={project.image}
+              alt={project.title}
+            />
+          </GridItem>
+          <GridItem colSpan={{ base: 12, md: 8, lg: 7 }}>
+            <Heading mb={10} fontSize={'lg'}>
+              <Box
+                as="span"
+                position={'relative'}
+                color={useColorModeValue(
+                  ColorScheme.light.primary,
+                  ColorScheme.dark.primary,
+                )}
+              >
+                {project.title}
                 <Box
-                  as="span"
-                  position={'relative'}
-                  color={useColorModeValue(
-                    ColorScheme.light.primary,
-                    ColorScheme.dark.primary,
+                  position={'absolute'}
+                  borderBottom={useColorModeValue(
+                    `2px solid ${ColorScheme.light.primary}`,
+                    `2px solid ${ColorScheme.dark.primary}`,
                   )}
-                >
-                  {project.title}
-                  <Box
-                    position={'absolute'}
-                    borderBottom={useColorModeValue(
-                      `2px solid ${ColorScheme.light.primary}`,
-                      `2px solid ${ColorScheme.dark.primary}`,
-                    )}
-                    bottom={0}
-                    left={'100%'}
-                    right={0}
-                    transition={'.5s'}
-                    _groupHover={{
-                      left: '25%',
-                    }}
-                  ></Box>
-                </Box>
-              </Heading>
-
-              <Box mb={5}>
-                <Heading fontSize={'md'} mb={2}>
-                  Overview
-                </Heading>
-                <Content>{project.description}</Content>
+                  bottom={0}
+                  left={'100%'}
+                  right={0}
+                  transition={'.5s'}
+                  _groupHover={{
+                    left: '25%',
+                  }}
+                ></Box>
               </Box>
-            </GridItem>
-          </Grid>
-
-          <Box my={2}>
-            <Heading fontSize={'md'} mb={2}>
-              Tools
             </Heading>
-            <Content>{project.description}</Content>
-          </Box>
 
-          <HStack justifyContent={'end'} gap={5}>
-            <Button rounded={0} size={'sm'}>
-              Source Code
-              <Box pl={2} as="span">
-                {' '}
-                <Code />
-              </Box>
-            </Button>
-            <Button
-              bg={useColorModeValue(
-                ColorScheme.light.primary,
-                ColorScheme.dark.primary,
-              )}
-              color={useColorModeValue(
-                ColorScheme.dark.text,
-                ColorScheme.light.text,
-              )}
-              w={'50%'}
-              rounded={0}
-              size={'sm'}
-              textAlign={'end'}
-            >
-              Preview{' '}
-              <Box pl={2} as="span">
-                {' '}
-                <PlayOne />
-              </Box>
-            </Button>
-          </HStack>
-        </CardBody>
-      </Card>
-    </Box>
+            <Box mb={5}>
+              <Heading fontSize={'md'} mb={2}>
+                Overview
+              </Heading>
+              <Content>{project.description}</Content>
+            </Box>
+          </GridItem>
+        </Grid>
+
+        <Box my={2}>
+          <Heading fontSize={'md'} mb={2}>
+            Tools
+          </Heading>
+          <List>
+            {project.stack.map((tool, i) => (
+              <HStack gap={2} key={i}>
+                <ListIcon as={() => <ArrowRight />} />
+                <Box>{tool}</Box>
+              </HStack>
+            ))}
+          </List>
+        </Box>
+
+        <HStack justifyContent={'end'} gap={5}>
+          <Button
+            href={project.source_code_link}
+            as={'a'}
+            target="_blank"
+            rounded={0}
+            size={'sm'}
+          >
+            Source Code
+            <Box pl={2} as="span">
+              {' '}
+              <Code />
+            </Box>
+          </Button>
+          <Button
+            href={project.preview_link}
+            target="_blank"
+            as={'a'}
+            bg={useColorModeValue(
+              ColorScheme.light.primary,
+              ColorScheme.dark.primary,
+            )}
+            color={useColorModeValue(
+              ColorScheme.dark.text,
+              ColorScheme.light.text,
+            )}
+            w={'50%'}
+            rounded={0}
+            size={'sm'}
+            textAlign={'end'}
+          >
+            Preview{' '}
+            <Box pl={2} as="span">
+              {' '}
+              <PlayOne />
+            </Box>
+          </Button>
+        </HStack>
+      </CardBody>
+    </Card>
   )
 }
 
